21ba1a8fdfc21f827c4178026e2a54327bf1c09a
Ariba <ariba.madakia@gmail.com>
2024-10-29 19:04:38 +0530

    Added Another Button

--- src/app/mini-projects/buttons/page.tsx
+++ src/app/mini-projects/buttons/page.tsx
@@ -5,8 +5,8 @@ export default function ButtonsPage() {
     <div className="container mx-auto p-12">
       {" "}
       <h1 className="text-3xl font-bold mb-8 text-center">Buttons</h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="border border-gray-300 p-4 rounded-lg flex items-center justify-center w-full h-24">
           <a
             href="#_"
             className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group cursor-pointer"
@@ -14,6 +14,51 @@ export default function ButtonsPage() {
             <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
             <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
             <span className="relative">Hover Me!</span>
+          </a>
+        </div>
+
+        <div className="border border-gray-300 p-4 rounded-lg flex items-center justify-center w-full h-24">
+          <a
+            href="#_"
+            className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-white transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-800 group"
+          >
+            <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-600 group-hover:h-full"></span>
+            <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+              <svg
+                className="w-5 h-5 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+
+            <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+              <svg
+                className="w-5 h-5 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+              Hover Me!
+            </span>
           </a>
         </div>
       </div>
